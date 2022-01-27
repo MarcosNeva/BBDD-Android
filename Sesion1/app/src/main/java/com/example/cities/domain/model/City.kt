41718fc945e5795dd755463a5eff0583d221d9d6
ZkravenOnLaptop <marcosneva@icloud.com>
2022-01-27 16:45:33 +0100

    feature/SharedPreferences practice completed

--- Sesion1/app/src/main/java/com/example/cities/domain/model/City.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cities.domain.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class City(val id: Int, val name: String, val description: String, val sunshineHours: Int) :Parcelable
--- Sesion1/app/src/main/java/com/example/cities/domain/model/CityFilter.kt
+++
@@ -1,3 +0,0 @@
-package com.example.cities.domain.model
-
-enum class CityFilter { ALL_CITIES, SUNNY_CITIES, CLOUDY_CITIES }
